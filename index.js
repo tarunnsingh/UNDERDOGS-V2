bfdd3aadd943622cd82592ddd4139dd2dce38090
unknown <pande.akshat21@gmail.com>
2018-05-12 02:29:58 +0530

    second commit to continue the rough sketch

---
+++ index.js
@@ -0,0 +1,8 @@
+if(screen.width>800)
+{
+  document.getElementsByClassName("mobile")[0].style.display="none";
+}
+else{
+  document.getElementsByClassName("big-screens")[0].style.display="none";
+  document.getElementsByClassName("big-screens")[1].style.display="none";
+}
